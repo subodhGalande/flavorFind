24b5a42e9dcf1955af423b05eac3bca2dfcbbd1e
Subodh Galande <subodh.dsgn@gmail.com>
2024-11-11 18:46:19 +0530

    add: loading state to search button

--- src/components/searchCategory.jsx
+++ src/components/searchCategory.jsx
@@ -3,10 +3,16 @@ import { useState } from "react";
 
 const SearchCategory = () => {
   const [Search, setSearch] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleOpen = (e) => {
     e.preventDefault();
-    setSearch(true);
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setSearch(true);
+      setIsLoading(false);
+    }, 5000);
   };
 
   return (
@@ -25,12 +31,46 @@ const SearchCategory = () => {
             className="h-full w-[80%] outline-none placeholder:text-sm placeholder:font-light"
             placeholder="search recipes"
           />
-          <button
-            type="submit"
-            className="absolute bottom-1 right-1 z-20 rounded-lg bg-black px-3 py-1 text-white sm:bottom-[0.4rem] sm:right-2 sm:h-3/4 sm:px-4"
-          >
-            Search
-          </button>
+
+          {isLoading ? (
+            <button
+              className="absolute bottom-1 right-1 z-20 flex items-center gap-x-2 rounded-lg bg-black/50 px-3 py-1 text-white sm:bottom-[0.4rem] sm:right-2 sm:h-3/4 sm:px-4"
+              disabled
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+                  opacity="0.25"
+                />
+                <path
+                  fill="currentColor"
+                  d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+                >
+                  <animateTransform
+                    attributeName="transform"
+                    dur="0.75s"
+                    repeatCount="indefinite"
+                    type="rotate"
+                    values="0 12 12;360 12 12"
+                  />
+                </path>
+              </svg>{" "}
+              searching...
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="absolute bottom-1 right-1 z-20 rounded-lg bg-black px-3 py-1 text-white sm:bottom-[0.4rem] sm:right-2 sm:h-3/4 sm:px-4"
+            >
+              Search
+            </button>
+          )}
         </form>
         <div className="mx-auto my-4 flex w-full flex-wrap items-center justify-evenly gap-x-4 gap-y-2 sm:justify-center">
           <button className="rounded-lg border px-2 py-1 text-sm">
